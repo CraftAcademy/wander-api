92a10f28c620237cad8a8e6c93522a9e28daadc3
piavw <piavonwachenfelt@hotmail.com>
2019-11-12 13:37:10 +0100

    Fixes indentation.

--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Trail, type: :model do
   describe 'DB table' do
-  it { is_expected.to have_db_column :title }
-  it { is_expected.to have_db_column :description }
-  it { is_expected.to have_db_column :intensity }
-  it { is_expected.to have_db_column :duration }
-  it { is_expected.to have_db_column :location }
-  it { is_expected.to have_db_column :extra }
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :intensity }
+    it { is_expected.to have_db_column :duration }
+    it { is_expected.to have_db_column :location }
+    it { is_expected.to have_db_column :extra }
   end
   
   describe 'Validation' do
@@ -16,7 +16,6 @@ RSpec.describe Trail, type: :model do
     it { is_expected.to validate_presence_of :intensity }
     it { is_expected.to validate_presence_of :duration }
     it { is_expected.to validate_presence_of :location }
-
   end
 
   describe 'Factory' do
