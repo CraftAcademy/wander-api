af50f0aed1556c6140bfcd878605ca7f530539a1
piavw <piavonwachenfelt@hotmail.com>
2019-11-12 17:40:44 +0100

    Creates request and spec test for image attachment - Creates fixture folder, adds testimage - Creates new request test - Updates factory - Updates trail model

--- spec/factories/trails.rb
+++ spec/factories/trails.rb
@@ -6,5 +6,11 @@ FactoryBot.define do
     extra {'Warning'}
     duration { 60 }
     location {'MyArea'} 
+
+    after(:create) do |trail|
+      trail.image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'testimage.png')),
+      filename: 'attachment.png',
+      content_type: 'image/png')
+    end
   end
 end
