c53b8fd9a5f171d215238cc5a0cab31cdbbe8188
piavw <piavonwachenfelt@hotmail.com>
2019-11-13 10:35:39 +0100

    Updates seedsfile - removes curly-brackets.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ Trail.destroy_all
 
 7.times do 
   Trail.create(
-    title: {Faker::Games::ElderScrolls.region}, 
-    description: {Faker::Lorem.sentence(word_count: 16)}, 
-    intensity: {Faker::Number.between(from: 1, to: 5)}, 
-    extra: {Faker::Lorem.sentence}, 
-    duration: {Faker::Number.between(from: 10, to: 300)}, 
-    location: {Faker::Address.city}
+    title: Faker::Games::ElderScrolls.region, 
+    description: Faker::Lorem.sentence(word_count: 16), 
+    intensity: Faker::Number.between(from: 1, to: 5), 
+    extra: Faker::Lorem.sentence, 
+    duration: Faker::Number.between(from: 10, to: 300), 
+    location: Faker::Address.city
   )
 end
