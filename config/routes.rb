0f23ca946455dc9b1006fffb53c0b6ef7e66d2c0
luca <l.lobacher@web.de>
2019-11-13 17:39:09 +0100

    Moves user routes outside namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   namespace :v1, defaults: { format: :json } do
     resources :trails, only: [:index, :create, :show]
-    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   end
 end
