c16928ccc1696b3d1dfcd75e295f055f60e6b172
piavw <piavonwachenfelt@hotmail.com>
2019-11-14 11:24:10 +0100

    Creates migration for latitude & longitude
    Co-Authored-By: Alex Saxena, <saxenaalex@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_173650) do
+ActiveRecord::Schema.define(version: 2019_11_14_100713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2019_11_13_173650) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
   end
 
   create_table "users", force: :cascade do |t|
