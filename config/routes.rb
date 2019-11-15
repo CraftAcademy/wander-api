7bcd65c09909c33112f6b7d5894680e9ccafc63d
piavw <piavonwachenfelt@hotmail.com>
2019-11-15 12:38:40 +0100

    Debugging through visualising a test search in view.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :trails, only: [:index, :create, :show]
   end
+  root to: 'search#search'
+  resources :search
 end
