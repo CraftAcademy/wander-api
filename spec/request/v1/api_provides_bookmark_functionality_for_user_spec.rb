83871558dbdeb61f73e6ea2b7ac9b41c6bfe34d1
piavw <piavonwachenfelt@hotmail.com>
2019-11-16 17:16:54 +0100

    Creates request tests for bookmark functionality
    Co-Authored-By: Alex Saxena, <saxenaalex@gmail.com>

--- spec/request/v1/api_provides_bookmark_functionality_for_user_spec.rb
+++ spec/request/v1/api_provides_bookmark_functionality_for_user_spec.rb
@@ -1,1 +1,26 @@
-RSpec
+RSpec.describe 'API provides bookmark functionality' do 
+  describe 'user can successfully bookmark a trail' do 
+    let!(:trail) { create(:trail) } 
+    let(:user) { create(:user) }
+    let(:credentials) { user.create_new_auth_token}
+    let(:headers) {{ HTTP_ACCEPT: "application/json" }.merge!(credentials)}
+
+    before do
+      get '/v1/trails'
+      post '/v1/bookmark/create',
+        params: {
+          user: user.id,
+          trail: trail.id
+        },
+        headers: headers
+    end
+   
+    it 'successful bookmark' do 
+      expect(response_json).to eq 'unknown'
+    end
+
+    it 'gives 200 status' do 
+      expect(reponse.status).to eq 200
+    end
+  end
+end
