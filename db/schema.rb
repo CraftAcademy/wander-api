1adac76bf4ca60b080f13bee6ddd1111562ff085
piavw <piavonwachenfelt@hotmail.com>
2019-11-16 15:11:09 +0100

    Creates test for bookmark model.

--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 2019_11_16_131233) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude", null: false
-    t.float "longitude", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.bigint "user_id"
     t.string "continent"
     t.index ["user_id"], name: "index_trails_on_user_id"
---
+++ spec/models/bookmark_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Bookmark, type: :model do
+  describe 'Association' do
+    it { is_expected.to belong_to :user }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:bookmark)).to be_valid
+    end
+  end 
+end
