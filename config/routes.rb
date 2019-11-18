c51365e4232b25e3c2956b3c90071f4f644a86ad
piavw <piavonwachenfelt@hotmail.com>
2019-11-18 18:06:09 +0100

    Refactors bookmarks controller and routes
    - Adds destroy action
    - Updates render JSON with collectionserializer to all bookmark actions (index, create & destroy)
    - Adds error-messages
    
    Co-Authored-By: Alex Saxena, <saxenaalex@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   namespace :v1, defaults: { format: :json } do
     resources :trails, only: [:index, :create, :show]
-    resources :bookmarks, only: [:create, :index]
+    resources :bookmarks, only: [:create, :index, :destroy]
   end
 end
