48597937caf5f90def65cbe66aa4cdce73b67acc
piavw <piavonwachenfelt@hotmail.com>
2019-11-18 15:28:11 +0100

    Updates trail and user Model associations in
    Co-Authored-By: Alex Saxena, <saxenaalex@gmail.com>

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -3,6 +3,7 @@ class Trail < ApplicationRecord
   validates :title, length: { minimum: 5 }
   validates :description, length: { minimum: 15 }
   has_one_attached :image
-
   belongs_to :user
+
+  has_many :bookmarks, through: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
-
   has_many :trails
+
   has_many :bookmarks, through: :trails
 end
