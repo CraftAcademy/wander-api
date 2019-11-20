f450e1db3acfbcee2d4cf69dd36b0fc5e6674f6d
piavw <piavonwachenfelt@hotmail.com>
2019-11-20 14:12:42 +0100

    Creates request-test for search, fixes spellingerror in bookmarks-controller.

---
+++ spec/request/v1/api_provides_search_functionality_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe 'API provides search functionality', type: :request do
+  describe 'displays search results' do
+    let!(:trail_1) { create(:trail, title: 'Stockholm Hagaparken Trail') }
+    let!(:trail_2) { create(:trail, city: 'Stockholm') }
+    
+    before do
+      get '/v1/trails?search=Stockholm'
+    end
+   
+    it 'returns 2 trails' do
+      expect(response_json.count).to eq 2
+    end 
+
+    it 'trail has search query within title' do
+      expect(response_json.first['title']).to include 'Stockholm'
+    end
+
+    it 'trail has search query as city' do
+      expect(response_json.last['city']).to include 'Stockholm'
+    end
+  end
+
+  describe 'returns error if there are no trails' do
+    before do
+      get '/v1/trails?search'
+    end
+
+    it 'returns error status' do
+      expect(response.status).to eq 400
+    end
+
+    it 'returns error message' do
+      expect(response_json['error_message']).to eq 'Needs to be 2 or more letters'
+    end
+  end
+end
